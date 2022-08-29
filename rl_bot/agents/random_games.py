db4885eb0de46fdf8558018107c577276c3b7073
Joichiro433 <joichiro322@gmail.com>
2022-08-29 23:49:14 +0900

    updata: dataclassによる型付け

--- rl_bot/agents/random_games.py
+++ rl_bot/agents/random_games.py
@@ -3,13 +3,13 @@ import random
 import numpy as np
 from rich import print
 
-from environments.trading_env import TradingEnv, Action
+from environments.trading_env import TradingEnv, Action, State
 
 
 def random_games(env: TradingEnv, train_episodes: int = 50, training_batch_size: int = 500) -> None:
     average_net_worth: float = 0
     for episode in range(train_episodes):
-        state = env.reset(env_steps_size = training_batch_size)
+        state: State = env.reset(env_steps_size = training_batch_size)
 
         while True:
             env.render()
--- rl_bot/environments/trading_env.py
+++ rl_bot/environments/trading_env.py
@@ -1,3 +1,4 @@
+from sys import float_repr_style
 from typing import List, Tuple, Deque, Any
 import random
 from collections import deque
@@ -9,7 +10,30 @@ import numpy as np
 import pandas as pd
 from rich import print
 
-State = Any
+
+@dataclass(frozen=True)
+class Market:
+    open: float
+    high: float
+    low: float
+    close: float
+    volume: float
+
+
+@dataclass(frozen=True)
+class Order:
+    balance: float
+    net_worth: float
+    crypto_bought: float
+    crypto_sold: float
+    crypto_held: float
+
+
+@dataclass(frozen=True)
+class State:
+    market_history: Deque[Market]
+    orders_history: Deque[Order]
+
 
 class Action(Enum):
     BUY = 1
@@ -45,10 +69,10 @@ class TradingEnv:
         self.action_space = np.array([0, 1, 2])
 
         # Orders history contains the balance, net_worth, crypto_bought, crypto_sold, crypto_held values for the last lookback_window_size steps
-        self.orders_history: Deque = deque(maxlen=self.lookback_window_size)
+        self.orders_history: Deque[Order] = deque(maxlen=self.lookback_window_size)
         
         # Market history contains the OHCL values for the last lookback_window_size prices
-        self.market_history: Deque = deque(maxlen=self.lookback_window_size)
+        self.market_history: Deque[Market] = deque(maxlen=self.lookback_window_size)
 
         # State size contains Market+Orders history for the last lookback_window_size steps
         self.state_size: Tuple[int, int] = (self.lookback_window_size, 10)
@@ -72,26 +96,34 @@ class TradingEnv:
 
         for i in reversed(range(self.lookback_window_size)):
             current_step: int = self.current_step - i
-            self.orders_history.append([self.balance, self.net_worth, self.crypto_bought, self.crypto_sold, self.crypto_held])
-            self.market_history.append([self.df.loc[current_step, 'Open'],
-                                        self.df.loc[current_step, 'High'],
-                                        self.df.loc[current_step, 'Low'],
-                                        self.df.loc[current_step, 'Close'],
-                                        self.df.loc[current_step, 'Volume']
-                                        ])
+            order: Order = Order(
+                balance=self.balance, 
+                net_worth=self.net_worth, 
+                crypto_bought=self.crypto_bought,
+                crypto_sold=self.crypto_sold,
+                crypto_held=self.crypto_held)
+            self.orders_history.append(order)
+            market: Market = Market(
+                open=self.df.loc[current_step, 'Open'],
+                high=self.df.loc[current_step, 'High'],
+                low=self.df.loc[current_step, 'Low'],
+                close=self.df.loc[current_step, 'Close'],
+                volume=self.df.loc[current_step, 'Volume'])
+            self.market_history.append(market)
 
-        state: State = np.concatenate((self.market_history, self.orders_history), axis=1)
+        state: State = State(market_history=self.market_history, orders_history=self.orders_history)
         return state
 
     # Get the data points for the given current_step
     def _next_observation(self) -> State:
-        self.market_history.append([self.df.loc[self.current_step, 'Open'],
-                                    self.df.loc[self.current_step, 'High'],
-                                    self.df.loc[self.current_step, 'Low'],
-                                    self.df.loc[self.current_step, 'Close'],
-                                    self.df.loc[self.current_step, 'Volume']
-                                    ])
-        obs: State = np.concatenate((self.market_history, self.orders_history), axis=1)
+        market: Market = Market(
+            open=self.df.loc[self.current_step, 'Open'],
+            high=self.df.loc[self.current_step, 'High'],
+            low=self.df.loc[self.current_step, 'Low'],
+            close=self.df.loc[self.current_step, 'Close'],
+            volume=self.df.loc[self.current_step, 'Volume'])
+        self.market_history.append(market)
+        obs: State = State(market_history=self.market_history, orders_history=self.orders_history)
         return obs
 
     # Execute one time step within the environment
@@ -121,7 +153,13 @@ class TradingEnv:
         self.prev_net_worth = self.net_worth
         self.net_worth = self.balance + self.crypto_held * current_price
 
-        self.orders_history.append([self.balance, self.net_worth, self.crypto_bought, self.crypto_sold, self.crypto_held])
+        order: Order = Order(
+            balance=self.balance, 
+            net_worth=self.net_worth, 
+            crypto_bought=self.crypto_bought,
+            crypto_sold=self.crypto_sold,
+            crypto_held=self.crypto_held)
+        self.orders_history.append(order)
 
         reward: float = self.net_worth - self.prev_net_worth
         done: bool = self.net_worth <= self.initial_balance/2
